3dbd738848c216544255e9d4f3cc8b11c1f11453
Harikalyan <harikalyan99tronics@gmail.com>
2024-11-13 22:49:35 +0530

    minor changes

--- index.js
+++ index.js
@@ -9,38 +9,63 @@ let tempPassword = "";
 })();
 
 document.querySelector("#username").addEventListener("input", (event) => {
-  usernameFn(event.target.value) && event.target.value?.length ?  document.getElementById("usernameLabel").innerText = `Username: ${event.target.value} can't be less than 4 characters` : document.getElementById("usernameLabel").innerText = ""
+  usernameFn(event.target.value) && event.target.value?.length
+    ? (document.getElementById(
+        "usernameLabel"
+      ).innerText = `Username: ${event.target.value} can't be less than 4 characters`)
+    : (document.getElementById("usernameLabel").innerText = "");
 });
 
 document.querySelector("#name").addEventListener("input", (event) => {
-  nameFn(event.target.value) && event.target.value?.length ?  document.getElementById("nameLabel").innerText = `Name can't be numbers` : document.getElementById("nameLabel").innerText = ""
+  nameFn(event.target.value) && event.target.value?.length
+    ? (document.getElementById("nameLabel").innerText = `Name can't be numbers`)
+    : (document.getElementById("nameLabel").innerText = "");
 });
 
 document.querySelector("#mail").addEventListener("input", (event) => {
-  if(emailFn(event.target.value).mailLength && event.target.value?.length || emailFn(event.target.value).mailChar && event.target.value?.length){
-     if (emailFn(event.target.value).mailChar) {
-      return document.getElementById("mailLabel").innerText = "Email must contain the symbol @";
-    }else if (emailFn(event.target.value).mailLength) {
-      return document.getElementById("mailLabel").innerText = "Email must be at least 6 characters";
-    } 
-  } else{return document.getElementById("mailLabel").innerText = "";}
+  if (
+    (emailFn(event.target.value).mailLength && event.target.value?.length) ||
+    (emailFn(event.target.value).mailChar && event.target.value?.length)
+  ) {
+    if (emailFn(event.target.value).mailChar) {
+      return (document.getElementById("mailLabel").innerText =
+        "Email must contain the symbol @");
+    } else if (emailFn(event.target.value).mailLength) {
+      return (document.getElementById("mailLabel").innerText =
+        "Email must be at least 6 characters");
+    }
+  } else {
+    return (document.getElementById("mailLabel").innerText = "");
+  }
 });
 
 document.querySelector("#phone").addEventListener("input", (event) => {
-  if(phoneFn(event.target.value).notNumbered && event.target.value?.length || phoneFn(event.target.value).phoneLength && event.target.value?.length){
-     if (phoneFn(event.target.value).notNumbered) {
-      return document.getElementById("phoneLabel").innerText = "Phone number can olny be number";
-    }else if (phoneFn(event.target.value).phoneLength) {
-      return document.getElementById("phoneLabel").innerText = "Length of phone number can't be less than 7";
-    } 
-  } else{return document.getElementById("phoneLabel").innerText = "";}
+  if (
+    (phoneFn(event.target.value).notNumbered && event.target.value?.length) ||
+    (phoneFn(event.target.value).phoneLength && event.target.value?.length)
+  ) {
+    if (phoneFn(event.target.value).notNumbered) {
+      return (document.getElementById("phoneLabel").innerText =
+        "Phone number can olny be number");
+    } else if (phoneFn(event.target.value).phoneLength) {
+      return (document.getElementById("phoneLabel").innerText =
+        "Length of phone number can't be less than 7");
+    }
+  } else {
+    return (document.getElementById("phoneLabel").innerText = "");
+  }
 });
 
-
-document.querySelector("#confirmpassword").addEventListener("input", (event) => {
-  tempPassword = document.querySelector("#password").value
-  passwordFn(event.target.value) && event.target.value?.length ?  document.getElementById("confirmpasswordLabel").innerText = `Password and confirm password must be same.` : document.getElementById("confirmpasswordLabel").innerText = ""
-});
+document
+  .querySelector("#confirmpassword")
+  .addEventListener("input", (event) => {
+    tempPassword = document.querySelector("#password").value;
+    passwordFn(event.target.value) && event.target.value?.length
+      ? (document.getElementById(
+          "confirmpasswordLabel"
+        ).innerText = `Password and confirm password must be same.`)
+      : (document.getElementById("confirmpasswordLabel").innerText = "");
+  });
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -52,27 +77,16 @@ form.addEventListener("submit", (event) => {
   tempPassword = password;
   let confirmPassword = event.target["confirmpassword"].value;
 
-  if (usernameFn(userName)) {
-    alert(`Username: ${userName} can't be less than 4 characters`);
-  } else if (nameFn(fullName)) {
-    alert("Name can't be numbers");
-  } else if (emailFn(mail).mailLength || emailFn(mail).mailChar) {
-    if (emailFn(mail).mailLength) {
-      alert("Email must be at least 6 characters");
-    } else if (emailFn(mail).mailChar) {
-      alert("Email must contain the symbol @");
-    }
-  } else if (
+  if (
+    usernameFn(userName) ||
+    nameFn(fullName) ||
+    emailFn(mail).mailLength ||
+    emailFn(mail).mailChar ||
     phoneFn(phoneNumber).notNumbered ||
-    phoneFn(phoneNumber).phoneLength
+    phoneFn(phoneNumber).phoneLength ||
+    passwordFn(confirmPassword)
   ) {
-    if (phoneFn(phoneNumber).notNumbered) {
-      alert("Phone number can olny be number");
-    } else if (phoneFn(phoneNumber).phoneLength) {
-      alert("Length of phone number can't be less than 7");
-    }
-  } else if (passwordFn(confirmPassword)) {
-    alert("Password and confirm password must be same.");
+    alert("Invalid credentials");
   } else {
     alert(`User added successfully` + `${"✅"}`);
     location.reload();
@@ -99,10 +113,9 @@ function emailFn(mail) {
   };
   if (!mail.includes("@")) {
     obj.mailChar = true;
-  }
-  else if (mail.length < 6) {
+  } else if (mail.length < 6) {
     obj.mailLength = true;
-  } 
+  }
   return obj;
 }
 
