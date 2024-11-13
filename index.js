b02beade28c009140e26db532730ac1d44e5c01d
Harikalyan <harikalyan99tronics@gmail.com>
2024-11-13 20:17:17 +0530

    making it so strong

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 let form = document.querySelector(".form-container");
 let tempPassword = "";
 
-(function opening(){
-    setTimeout(() => {
-        document.querySelector(".lazy-loading").className = "d-none";
-        document.querySelector("#show").className = "";
-    }, 4000)
-})()
+(function opening() {
+  setTimeout(() => {
+    document.querySelector(".lazy-loading").className = "d-none";
+    document.querySelector("#show").className = "";
+  }, 4000);
+})();
+
+document.querySelector("#username").addEventListener("input", (event) => {
+  usernameFn(event.target.value) && event.target.value?.length ?  document.getElementById("usernameLabel").innerText = `Username: ${event.target.value} can't be less than 4 characters` : document.getElementById("usernameLabel").innerText = ""
+});
+
+document.querySelector("#name").addEventListener("input", (event) => {
+  nameFn(event.target.value) && event.target.value?.length ?  document.getElementById("nameLabel").innerText = `Name can't be numbers` : document.getElementById("nameLabel").innerText = ""
+});
+
+document.querySelector("#mail").addEventListener("input", (event) => {
+  if(emailFn(event.target.value).mailLength && event.target.value?.length || emailFn(event.target.value).mailChar && event.target.value?.length){
+     if (emailFn(event.target.value).mailChar) {
+      return document.getElementById("mailLabel").innerText = "Email must contain the symbol @";
+    }else if (emailFn(event.target.value).mailLength) {
+      return document.getElementById("mailLabel").innerText = "Email must be at least 6 characters";
+    } 
+  } else{return document.getElementById("mailLabel").innerText = "";}
+});
+
+document.querySelector("#phone").addEventListener("input", (event) => {
+  if(phoneFn(event.target.value).notNumbered && event.target.value?.length || phoneFn(event.target.value).phoneLength && event.target.value?.length){
+     if (phoneFn(event.target.value).notNumbered) {
+      return document.getElementById("phoneLabel").innerText = "Phone number can olny be number";
+    }else if (phoneFn(event.target.value).phoneLength) {
+      return document.getElementById("phoneLabel").innerText = "Length of phone number can't be less than 7";
+    } 
+  } else{return document.getElementById("phoneLabel").innerText = "";}
+});
 
 
+document.querySelector("#confirmpassword").addEventListener("input", (event) => {
+  tempPassword = document.querySelector("#password").value
+  passwordFn(event.target.value) && event.target.value?.length ?  document.getElementById("confirmpasswordLabel").innerText = `Password and confirm password must be same.` : document.getElementById("confirmpasswordLabel").innerText = ""
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -20,9 +52,9 @@ form.addEventListener("submit", (event) => {
   tempPassword = password;
   let confirmPassword = event.target["confirmpassword"].value;
 
-  if (userNameFn(userName)) {
+  if (usernameFn(userName)) {
     alert(`Username: ${userName} can't be less than 4 characters`);
-  } else if (fullNameFn(fullName)) {
+  } else if (nameFn(fullName)) {
     alert("Name can't be numbers");
   } else if (emailFn(mail).mailLength || emailFn(mail).mailChar) {
     if (emailFn(mail).mailLength) {
@@ -31,12 +63,12 @@ form.addEventListener("submit", (event) => {
       alert("Email must contain the symbol @");
     }
   } else if (
-    phoneNumberFunction(phoneNumber).notNumbered ||
-    phoneNumberFunction(phoneNumber).phoneLength
+    phoneFn(phoneNumber).notNumbered ||
+    phoneFn(phoneNumber).phoneLength
   ) {
-    if (phoneNumberFunction(phoneNumber).notNumbered) {
+    if (phoneFn(phoneNumber).notNumbered) {
       alert("Phone number can olny be number");
-    } else if (phoneNumberFunction(phoneNumber).phoneLength) {
+    } else if (phoneFn(phoneNumber).phoneLength) {
       alert("Length of phone number can't be less than 7");
     }
   } else if (passwordFn(confirmPassword)) {
@@ -47,11 +79,11 @@ form.addEventListener("submit", (event) => {
   }
 });
 
-function userNameFn(username) {
+function usernameFn(username) {
   return username.length < 4;
 }
 
-function fullNameFn(name) {
+function nameFn(name) {
   for (let i = 0; i < name.length; i++) {
     if (name[i] >= "0" && name[i] <= "9") {
       return true;
@@ -65,15 +97,16 @@ function emailFn(mail) {
     mailLength: false,
     mailChar: false,
   };
-  if (mail.length < 6) {
-    obj.mailLength = true;
-  } else if (!mail.includes("@")) {
+  if (!mail.includes("@")) {
     obj.mailChar = true;
   }
+  else if (mail.length < 6) {
+    obj.mailLength = true;
+  } 
   return obj;
 }
 
-function phoneNumberFunction(phoneNumber) {
+function phoneFn(phoneNumber) {
   let phoneObj = {
     phoneLength: false,
     notNumbered: false,
